76c10de0b5c72f1429a88a80f08de68072b709df
Abu Sayeed <sayeed@pop-os.localdomain>
2022-10-19 22:13:55 +0600

    added different routes data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { routes } from './Routes/Routes/Routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={routes}></RouterProvider>
     </div>
   );
